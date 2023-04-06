aedb0ee52f8e8a2945da3cb2fa65800b275ba662
Bob Sull <bob@sull.com>
2023-04-06 15:57:47 +0200

    Correction du roadmap chap. 2.18

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -7,6 +7,6 @@ from . import views
 app_name='catalogue'
 
 urlpatterns = [
-    path('artist/', views.artist.index, name='artist_index'),
-    path('artist/<int:artist_id>', views.artist.show, name='artist_show'),
+    path('artist/', views.artist.index, name='artist-index'),
+    path('artist/<int:artist_id>', views.artist.show, name='artist-show'),
 ]
